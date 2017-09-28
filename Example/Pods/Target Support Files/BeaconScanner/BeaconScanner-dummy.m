19f13e62ad2a79c75514607783fd967e52104294
William Kuo <kuotinyen@gmail.com>
2017-09-28 18:31:44 +0800

    add code to project

---
+++ Example/Pods/Target Support Files/BeaconScanner/BeaconScanner-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BeaconScanner : NSObject
+@end
+@implementation PodsDummy_BeaconScanner
+@end
---
+++ Example/Pods/Target Support Files/BeaconScanner/BeaconScanner-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double BeaconScannerVersionNumber;
+FOUNDATION_EXPORT const unsigned char BeaconScannerVersionString[];
+
